69e0d7d786c7cb8d9ee44127d9e855cdc4a5ea8c
SkullDude1994 <38791692+SkullDude1994@users.noreply.github.com>
2021-11-18 23:20:49 +0200

    Created some preliminary functions for front end Subgroup and Assembly item services.

---
+++ CRT_WebApp/Client/Services/SubGroupService/ISubGroupService.cs
@@ -0,0 +1,55 @@
+﻿using CRT_WebApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Client.Services.SubGroupService
+{
+    //---------------------------------------------------------------------------------------------------------//
+    /// <summary>
+    /// Interface for subgroup service for client
+    /// </summary>
+    public interface ISubGroupService
+    {
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Register events to be invoked when subgroup list have been loaded/modified
+        /// </summary>
+        event Action OnChange;
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Current list of SubGroups items 
+        /// </summary>
+        List<SubGroupModel> SubGroups { get; set; }
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Adds the Subgroup to the  list
+        /// </summary>
+        /// <param name="subGroup">The subgroup to be added</param>
+        void AddSubGroupToList(SubGroupModel subGroup);
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Removes a subgroup from the list
+        /// </summary>
+        /// <param name="subGroup">The subgroup to be removed</param>
+        void RemoveSubGroupFromList(SubGroupModel subGroup);
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Adds a whole range of subgroups to the current list
+        /// </summary>
+        /// <param name="subGroups">The list of subgroups to be added</param>
+        void AddRangeOfSubGroups(IEnumerable<SubGroupModel> subGroups);
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Clears the subgroup list
+        /// </summary>
+        void ClearSubGroupList();
+        //---------------------------------------------------------------------------------------------------------//
+        /// <summary>
+        /// Gets the current list of subgroup items
+        /// </summary>
+        /// <returns>A list of subgroups</returns>
+        List<SubGroupModel> GetSubGroupList();
+    }
+}
+//-------------------------------------...ooo000 END OF FILE 000ooo...-------------------------------------//
