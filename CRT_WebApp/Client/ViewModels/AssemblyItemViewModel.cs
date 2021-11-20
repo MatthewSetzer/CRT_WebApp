1bf588a98d36c1e7677e283ad023801c2b34603f
SkullDude1994 <38791692+SkullDude1994@users.noreply.github.com>
2021-11-20 13:35:14 +0200

    Tested out using a view model for validation and using the built in blazor edit form components to handle double input values

---
+++ CRT_WebApp/Client/ViewModels/AssemblyItemViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+    //---------------------------------------------------------------------------------------------------------//
+    public class AssemblyItemViewModel
+    {
+        [Required]
+        [StringLength(100, MinimumLength =3)]
+        public string Description { get; set; }
+        public double Quantity { get; set; }
+        public double Rate { get; set; }
+    }
+}
+//-------------------------------------...ooo000 END OF FILE 000ooo...-------------------------------------//
