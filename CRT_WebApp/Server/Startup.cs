0e4b8ef4ba5cde6bd06a87b8c586fd469d30adbe
samelk <79966901+samelk@users.noreply.github.com>
2021-11-21 17:58:39 +0200

    Scaffolding

--- CRT_WebApp/Server/Startup.cs
+++ CRT_WebApp/Server/Startup.cs
@@ -74,7 +74,7 @@ namespace CRT_WebApp.Server
 
             app.UseRouting();
 
-            app.UseIdentityServer();
+            //app.UseIdentityServer();
             app.UseAuthentication();
             app.UseAuthorization();
 
