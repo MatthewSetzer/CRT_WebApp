f7aeb38b4d03ddc6e89730b76ad36eda49ba1b5b
MatthewSetzer <MatthewSetzer@users.noreply.github.com>
2021-11-22 13:03:38 +0200

    Finished functionality for creating sub-groups.

---
+++ CRT_WebApp/Client/ViewModels/SubGroupViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CRT_WebApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+    public class SubGroupViewModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public string SubGroupTitle { get; set; }
+        [Required]
+        public List<AssemblyItemModel> ListOfItems { get; set; }
+        [Required]
+        public double SubTotal { get; set; }
+
+        public string GetFormattedSubTotal() => SubTotal.ToString("0.00");
+    }
+}
