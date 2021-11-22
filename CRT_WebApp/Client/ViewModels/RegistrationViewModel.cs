7d88aa071fac2b22f3094750d12f54a3c10f541b
samelk <79966901+samelk@users.noreply.github.com>
2021-11-22 19:23:23 +0200

    RegistrationViewModel added. Formatting to files added.

---
+++ CRT_WebApp/Client/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+  public class RegistrationViewModel
+  {
+  }
+}
