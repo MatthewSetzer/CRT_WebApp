eeb6f20f4b9dd075ef631e2f0dfad4f834c170ff
samelk <79966901+samelk@users.noreply.github.com>
2021-11-22 19:21:23 +0200

    ForgotPassword Complete + ViewModels

---
+++ CRT_WebApp/Client/ViewModels/ForgotPasswordViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+  public class ForgotPasswordViewModel
+  {
+    [Required]
+    public string Email { get; set; }
+    public string Username { get; set; }
+  }
+}
---
+++ CRT_WebApp/Client/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+  public class LoginViewModel
+  {
+  }
+}
