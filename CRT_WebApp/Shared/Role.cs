b03904898af026915fa1d22cef8481aa467e3271
MatthewSetzer <MatthewSetzer@users.noreply.github.com>
2021-11-23 14:39:22 +0200

    Added Authorization for Admin and Employee and guarded Admin page

---
+++ CRT_WebApp/Shared/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Shared
+{
+    public enum Role
+    {
+        [Description("Admin")]
+        Admin,
+        [Description("Employee")]
+        Employee
+    }
+}
