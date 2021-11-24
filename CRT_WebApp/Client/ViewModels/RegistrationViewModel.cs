0ba87a5c3c4e77fc0b32043a3f0e708f86389fb5
samelk <79966901+samelk@users.noreply.github.com>
2021-11-24 17:48:11 +0200

    Finished Register

--- CRT_WebApp/Client/ViewModels/RegistrationViewModel.cs
+++ CRT_WebApp/Client/ViewModels/RegistrationViewModel.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRT_WebApp.Client.ViewModels
 {
   public class RegistrationViewModel
   {
+    [Required]
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
   }
 }
