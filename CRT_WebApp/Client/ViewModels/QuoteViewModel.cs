8f6f438374e6342c4b158da4c1d4e1eabf6d6457
MatthewSetzer <MatthewSetzer@users.noreply.github.com>
2021-11-24 13:59:23 +0200

    Create quote functionality

---
+++ CRT_WebApp/Client/ViewModels/QuoteViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Client.ViewModels
+{
+    public class QuoteViewModel
+    {
+        [Required]
+        public string QuoteTitle { get; set; }
+        [Required]
+        public bool QuoteState { get; set; }
+        [Required]
+        public DateTime QuoteDate { get; set; }
+        [Required]
+        public string QuoteUser { get; set; }
+        [Required]
+        public double Total { get; set; }
+        [Required]
+        public bool IsDeleted { get; set; }
+
+        public string GetFormattedTotal() => Total.ToString("0.00");
+
+    }
+}
