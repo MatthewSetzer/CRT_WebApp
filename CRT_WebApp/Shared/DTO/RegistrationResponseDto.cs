1810513cea3be521636b331f0f7113868c8b7df1
SkullDude1994 <38791692+SkullDude1994@users.noreply.github.com>
2021-11-29 12:19:54 +0200

    Added some dtos

---
+++ CRT_WebApp/Shared/DTO/RegistrationResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Shared.DTO
+{
+    internal class RegistrationResponseDto
+    {
+    }
+}
