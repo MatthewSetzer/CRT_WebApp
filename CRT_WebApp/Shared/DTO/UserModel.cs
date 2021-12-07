927b052347a14e68f1503d0b69ae11f976f579de
Samuel Dudley <samuel.dudley@gmail.com>
2021-12-07 14:07:43 +0200

    not much has changed

---
+++ CRT_WebApp/Shared/DTO/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRT_WebApp.Shared.DTO
+{
+    public class UserModel
+    {
+        public string Email { get; set; }
+        [EmailAddress]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(10)]
+        public string ConfirmPassword { get; set; }
+    }
+}
